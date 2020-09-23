b4e94793e2b9b5d471e4cfd09a45c9005474d107
Ankita Kar <ankitakar98@gmail.com>
2020-09-23 10:17:19 +0530

    Added Subtract method and tested

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,6 +1,10 @@
 public class Calculate {
 
-    public int sum(int a, int b) {
+    public int add(int a, int b) {
         return (a+b);
+    }
+
+    public int subtract(int a, int b) {
+        return (a-b);
     }
 }
--- src/TestCalculate.java
+++ src/TestCalculate.java
@@ -1,17 +1,52 @@
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 
 public class TestCalculate {
 
     @Test
-    public void testSum() {
+    public void testAdd() {
+        System.out.println("testAdd method");
+        //arrange
         Calculate calci = new Calculate();
         int a = 10;
         int b = 20;
         int expected = 30;
-
-        int actual = calci.sum(a,b);
-
+        //act
+        int actual = calci.add(a,b);
+        //assert
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSubtract() {
+        System.out.println("testSubtract method");
+        //arrange
+        Calculate calci = new Calculate();
+        int a = 55;
+        int b = 50;
+        int expected = 5;
+        //act
+        int actual = calci.subtract(a,b);
+        //assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Before
+    public void runBeforeEveryClass() {
+        System.out.println("Before every class");
+    }
+
+    @After
+    public void runAfterEveryClass() {
+        System.out.println("After every class");
+    }
+
+    @BeforeClass
+    public  static void runBeforeClass() {
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public  static void runAfterClass() {
+        System.out.println("After Class");
     }
 }
